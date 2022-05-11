3d8ec10b0f544999de8be5e371e4c817d4dc42a6
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-05-11 10:31:46 +0700

    Initial Project

---
+++ WebApplication4/Module/Employee.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Employee" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="WebApplication4.Module.Employee" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="d-flex justify-content-end form-group">
+        <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control" OnTextChanged="TextBox1_TextChanged"  ></asp:TextBox>
+    </div>
+        <asp:GridView
+            ID="empTable"
+            runat="server"
+            AutoGenerateColumns="False" 
+            Width="100%"
+            AllowCustomPaging="True"
+            AllowPaging="True"
+            OnPageIndexChanging="empTable_PageIndexChanging"
+            cssClass="table table-bordered">
+            <Columns>
+                <asp:BoundField HeaderText="EmployeeID" DataField="EmployeeID" />
+                <asp:BoundField HeaderText="EmployeeName" DataField="EmployeeName" />
+                <asp:BoundField HeaderText="Department" DataField="Department" />
+                <asp:BoundField HeaderText="Email" DataField="Email" />
+                <asp:BoundField HeaderText="Phone" DataField="Phone" />
+            </Columns>
+            <PagerSettings Mode="NumericFirstLast" FirstPageText="First" LastPageText="Last" />
+        </asp:GridView>
+</asp:Content>
+
---
+++ WebApplication4/Module/Employee.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication4.Data;
+
+namespace WebApplication4.Module
+{
+    public partial class Employee : Page
+    {
+        private readonly AdventureWorksDW2019Entities _db = new AdventureWorksDW2019Entities();
+        private static string search = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData(0, "");
+            }
+
+        }
+
+        private void LoadData(int pageIndex, string keysearch)
+        {
+            var query = _db.DimEmployees.AsQueryable();
+            keysearch = keysearch.Trim();
+            search = keysearch;
+            if (!string.IsNullOrEmpty(keysearch))
+            {
+                query = query.Where(a => a.DepartmentName.Contains(keysearch) || (a.Title + " " + a.FirstName + " " + a.LastName).Contains(keysearch));
+            }
+            var data = query.OrderBy(a => a.EmployeeKey).Skip((pageIndex) * 10).Take(10).Select(a => new
+            {
+                EmployeeID = a.EmployeeKey,
+                EmployeeName = a.Title + " " + a.FirstName + " " + a.LastName,
+                Department = a.DepartmentName,
+                Email = a.EmailAddress,
+                a.Phone
+            }).ToList();
+            empTable.VirtualItemCount = query.Count();
+            empTable.DataSource = data;
+            empTable.DataBind();
+        }
+
+        protected void empTable_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            empTable.PageIndex = e.NewPageIndex;
+            LoadData(e.NewPageIndex, search);
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            LoadData(1, TextBox1.Text);
+        }
+    }
+}
